193cbd7f734d653e47b11524333a1e36576d3668
Gonzalo <gonzalohellers@gmail.com>
2023-12-12 15:27:23 -0300

    Execute build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const morgan=require('morgan');
 app.use(express.json());
-
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[header] - :response-time ms'))
 const cors = require('cors')
 
