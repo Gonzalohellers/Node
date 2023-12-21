ebc0052956fbe7b5885d0c99f86e914163b9a580
Gonzalo <gonzalohellers@gmail.com>
2023-12-21 10:43:32 -0300

    Manejo del backend con base de datos

--- index.js
+++ index.js
@@ -1,98 +1,111 @@
 const http = require('http')
 const express = require('express');
 const app = express();
-const morgan=require('morgan');
+const morgan = require('morgan');
 app.use(express.json());
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[header] - :response-time ms'))
 const cors = require('cors')
+const Note = require('./models/note')
 
+require('dotenv').config()
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 app.use(cors())
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
-
+//import mongoose from 'mongoose'
+const mongoose = require('mongoose');
+const note = require('./models/note');
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
 })
 
-app.get('/api/info',(request, response)=>{
-  const argHora=new Date()
-  response.send("Tenemos Almacenadas un total de "+notes.length+ " notas <br> <br> Y la fecha es: " + argHora)
+app.get('/api/info', (request, response) => {
+  const argHora = new Date()
+  response.send("Tenemos Almacenadas un total de " + notes.length + " notas <br> <br> Y la fecha es: " + argHora)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const nota = notes.find(nota => nota.id === id)
-  if (nota) {
-    response.json(nota)
-  }
-  else {
-    response.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+
+
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+
 
 app.post('/api/notes', (request, response) => {
   const body = request.body;
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
   if (!body.content) {
     return response.status(400).json({
       error: 'content missing'
     });
   }
-  
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  };
+  try {
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
 
-  notes = notes.concat(note);
+    note
+      .save()
+      .then(savedNote => savedNote.toJSON())
+      .then(savedAndFormattedNote => {
+        response.json(savedAndFormattedNote)
+      }) 
+    }
+  catch(error) {
+      error => next(error)
+    }
+  })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-  response.json(note);
-});
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-  response.status(204).end()
-})
+  next(error)
+}
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.use(errorHandler)
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
 })
 
 
